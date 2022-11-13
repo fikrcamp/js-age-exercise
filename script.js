22a4bd277c5082970589e8055dc51b967368ec6c
AbduleYousuf <abduleyousuf08@gmail.com>
2022-11-13 21:45:45 +0300

    dogAge exercise solved

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hello");
+
+
+function calculateDogAge(age) {
+    return 7 * age
+}
+
+let dog1 = calculateDogAge(5)
+let dog2 = calculateDogAge(8)
+
+if (dog1 > dog2) {
+    console.log("dog1 is older than dog2  and his age is" + dog1 + " based on dog years")
+} else if (dog2 > dog1) {
+    console.log("dog2 is older than dog1  and his age is " + dog2 + " based on dog years")
+} else {
+    console.log("their are same")
+}
