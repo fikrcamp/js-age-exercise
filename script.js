5344f55d4b294d5160697ab0472aadfa6a2f1dd5
hamda <hamda.baashe44@gmail.com>
2022-11-14 18:01:15 +0000

    finished js-dog-age-exercise

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+
+const  calculateDogAge = (puppyAge) =>{
+    Age = puppyAge*7;
+    return console.log(`Your dog is ${Age} years old in dog years!`)
+
+}
+let dog1 = calculateDogAge(5);
+let dog2 = calculateDogAge(12);
+
