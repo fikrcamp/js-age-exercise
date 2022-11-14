c2d483ad22a0fab7945755539d9e2fbd9a683504
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2022-11-14 16:13:47 +0600

    js-age-exercise

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+//console.log("hello");
+
+function calculateDogAge(age){
+    console.log(`Your dog is ${age} years old in dog years! `)
+}
+calculateDogAge(30)
+calculateDogAge(37) //37
