b42846dc58c36b6364b4710bb8711661fc6d8579
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-16 11:57:20 +0300

    solved dog age calculater

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const calculateDogAge = puppysAge => { 
+
+  let dogAge = puppysAge * 7
+   console.log(`Your dog is ${dogAge} years old in dog years!`);
+}
+
+calculateDogAge(5);
+calculateDogAge(12);
+
+
+
+
