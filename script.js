467d015857cb6ccb16c899a3c65d8c3bb7d4e1b9
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-16 19:33:43 +0300

    js-age-exercise

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("hello");
+function calculate_dog_age(puppy_age){
+    console.log(`your dog age is ${puppy_age * 7} years in dog years!`);
+}
+calculate_dog_age(10)
+calculate_dog_age(12)
